2c25418151a445c10159bee18149f64d50089372
Stephen Roettger <stephen.roettger@gmail.com>
2020-01-24 22:16:34 +0100

    don' try to install kubectl if already exists

--- infrastructure/kubernetes/scripts/setup/login.sh
+++ infrastructure/kubernetes/scripts/setup/login.sh
@@ -4,6 +4,8 @@ set -Eeuo pipefail
 DIR="$( cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}")" )" >/dev/null && pwd )/../.."
 source $DIR/config/CLUSTER_VARS
 
-gcloud components install kubectl
+if [ ! -x $(which kubectl) ]; then
+  gcloud components install kubectl
+fi
 gcloud container clusters get-credentials ${CLUSTER_NAME}
 gcloud auth configure-docker
