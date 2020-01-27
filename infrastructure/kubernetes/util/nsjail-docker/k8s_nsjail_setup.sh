278080f9ef1865ded39ef14ed99b1b44e3a3b14d
Stephen Roettger <sroettger@google.com>
2020-01-27 15:53:32 +0100

    Create NSJAIL cgroup in an initContainer
    This allows us to only mount the cgroup directory itself as writable.
    Once we got rid of the clean /proc requirement, we can run the container
    unprivileged.

--- infrastructure/kubernetes/util/nsjail-docker/k8s_nsjail_setup.sh
+++ infrastructure/kubernetes/util/nsjail-docker/k8s_nsjail_setup.sh
@@ -6,8 +6,3 @@ set -Eeuxo pipefail
 mount -t tmpfs none /root
 mkdir /root/proc
 mount -t proc none /root/proc
-
-for res in cpu memory pids; do
-  mkdir -p "/sys/fs/cgroup/${res}/NSJAIL"
-  chmod 777 "/sys/fs/cgroup/${res}/NSJAIL"
-done
