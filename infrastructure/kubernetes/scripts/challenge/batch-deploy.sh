5453349d7ecdd1d900e3d135f46b065cca82cb8f
Stephen Roettger <stephen.roettger@gmail.com>
2020-01-28 22:45:42 +0100

    kctf-batch-deploy command

---
+++ infrastructure/kubernetes/scripts/challenge/batch-deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+DIR="$( cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}")" )" >/dev/null && pwd )/../.."
+source "${DIR}/scripts/lib/config.sh"
+
+load_config
+
+echo 'config loaded'
+echo "CLUSTER_NAME: ${CLUSTER_NAME}"
+
+for dir in ${CHAL_DIR}/*; do
+  if [ ! -f "${dir}/chal.conf" ]; then
+    continue
+  fi
+
+  CHALLENGE_NAME=$(basename "${dir}")
+  CHALLENGE_DIR=$(readlink -f "${CHAL_DIR}/${CHALLENGE_NAME}")
+  source "${CHALLENGE_DIR}/chal.conf"
+
+  if [ ! "${DEPLOY}" = "true" ]; then
+    continue
+  fi
+
+  echo
+  echo "= Deploying challenge ${CHALLENGE_NAME} ="
+  echo
+  ${DIR}/scripts/challenge/deploy.sh "${CHALLENGE_NAME}"
+done
