f580f37d78984746b20b52669118c907dfc2b793
Stephen Roettger <sroettger@google.com>
2020-01-29 14:12:09 +0100

    properly check cluster conf against chellenge dir

--- infrastructure/kubernetes/scripts/setup/challenge-directory.sh
+++ infrastructure/kubernetes/scripts/setup/challenge-directory.sh
@@ -9,30 +9,32 @@ if [ $# != 1 ]; then
     exit 1
 fi
 
-CHALLENGE_DIR=$1
+CHAL_DIR=$1
 
-if [ ! -d ${CHALLENGE_DIR} ]; then
-    if [ -e ${CHALLENGE_DIR} ]; then
-        echo "error: ${CHALLENGE_DIR} already exists and is not a directory"
+if [ ! -d ${CHAL_DIR} ]; then
+    if [ -e ${CHAL_DIR} ]; then
+        echo "error: ${CHAL_DIR} already exists and is not a directory"
         exit 1
     fi
-    echo "creating ${CHALLENGE_DIR}"
-    mkdir -p "${CHALLENGE_DIR}"
+    echo "creating ${CHAL_DIR}"
+    mkdir -p "${CHAL_DIR}"
 fi
 
-CHALLENGE_DIR=$(realpath -L ${CHALLENGE_DIR})
+CHAL_DIR=$(realpath -L ${CHAL_DIR})
 
 mkdir -p "${CONFIG_DIR}"
-ln -fs "${CHALLENGE_DIR}" "${CONFIG_DIR}/challenges"
+ln -fs "${CHAL_DIR}" "${CONFIG_DIR}/challenges"
 if [ -f "${CONFIG_DIR}/cluster.conf" ]; then
     source "${CONFIG_DIR}/cluster.conf"
-    # cluster.conf sets CHAL_DIR variable
-    # check if it's the same
-    if [ "${CHALLENGE_DIR}" != "${CHAL_DIR}" ]; then
-        echo 'unsetting cluster.conf from different challenge directory: ' $(readlink "${CONFIG_DIR}/cluster.conf")
+
+    CONFIG_PATH=$(readlink "${CONFIG_DIR}/cluster.conf")
+    generate_config_path
+    if [ "${ret}" != "${CONFIG_PATH}" ]; then
+        echo "unsetting cluster.conf from different challenge directory: ${CONFIG_PATH}"
         rm "${CONFIG_DIR}/cluster.conf"
     fi
 else
+    # remove dangling link
     if [ -L "${CONFIG_DIR}/cluster.conf" ]; then
         rm "${CONFIG_DIR}/cluster.conf"
     fi
