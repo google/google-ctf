e865e91b1d7714bb4a9c852addab43eec1a32747
Stephen Roettger <sroettger@google.com>
2020-02-06 16:33:11 +0100

    use value instead of csv in gcloud filter

--- infrastructure/kctf/scripts/cluster/resize.sh
+++ infrastructure/kctf/scripts/cluster/resize.sh
@@ -9,7 +9,7 @@ load_config
 gcloud config set project ${PROJECT}
 gcloud config set compute/zone ${ZONE}
 
-OLD_POOL_NAME=$(gcloud container node-pools list --cluster ctf-cluster-eu --format 'csv(name)' | tail -n+2)
+OLD_POOL_NAME=$(gcloud container node-pools list --cluster ctf-cluster-eu --format 'value(name)')
 if [ $(echo "${OLD_POOL_NAME}" | wc -l) != "1" ]; then
   echo 'error: this script expects exactly one node pool'
   echo '== node pools =='
