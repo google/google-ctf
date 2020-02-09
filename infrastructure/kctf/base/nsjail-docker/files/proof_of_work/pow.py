509d76de285f1d6dfa7f55918c5016e126b21d93
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2020-02-09 23:14:27 +0100

    Change the pow to have a predictable clock time
    Currently the pow is random depending on the seed, which is prone to precomputation, and to parallelization probing for a cheaper challenge. This pow is not parallelizable, isn't prone to precomputation or challenge selection, and has a deterministic clock time.

--- infrastructure/kctf/base/nsjail-docker/files/proof_of_work/pow.py
+++ infrastructure/kctf/base/nsjail-docker/files/proof_of_work/pow.py
@@ -1,124 +1,125 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import hmac
+import base64
 import secrets
-import sys
 import socket
+import sys
+
+VERSION = 's'
+MODULUS = 2**1279-1
+CHALSIZE = 2**128
 
 SOLVER_URL = 'https://github.com/google/google-ctf/blob/master/infrastructure/kctf/base/nsjail-docker/proof_of_work/pow.py'
 
 def stdin_is_localhost_socket():
     try:
         sock = socket.socket(fileno=sys.stdin.fileno())
+        peername = sock.getpeername()
+        sock.detach()
+        return peername[0] == '::ffff:127.0.0.1'
     except OSError:
         return False
 
-    peername = sock.getpeername()
-    sock.detach()
+def sloth_root(x, diff, p):
+    for i in range(diff):
+        x = pow(x, (p + 1) // 4, p) ^ 1
+    return x
 
-    return peername[0] == '::ffff:127.0.0.1'
+def sloth_square(y, diff, p):
+    for i in range(diff):
+        y = pow(y ^ 1, 2, p)
+    return y
 
-def gen_seed():
-    return secrets.token_hex(8)
+def encode_number(num):
+    size = (num.bit_length() // 24) * 3 + 3
+    return str(base64.b64encode(num.to_bytes(size, 'big')), 'utf-8')
 
-def byte_has_leading_zeroes(b, count):
-    if count == 0:
-        return True
+def decode_number(enc):
+    return int.from_bytes(base64.b64decode(bytes(enc, 'utf-8')), 'big')
 
-    for i in range(count):
-        if (b & 1) == 1:
-            return False
-        b >>= 1
+def decode_challenge(enc):
+    dec = enc.split('.')
+    if dec[0] != VERSION:
+        raise Exception('Unknown challenge version')
+    return list(map(decode_number, dec[1:]))
 
-    return True
+def encode_challenge(arr):
+    return '.'.join([VERSION] + list(map(encode_number, arr)))
 
-def has_leading_zeroes(inp, count):
-    for b in inp:
-        if count > 8:
-            if b != 0:
-                return False
-            count -= 8
-            continue
+def get_challenge(diff):
+    x = secrets.randbelow(CHALSIZE)
+    return encode_challenge([diff, x])
 
-        return byte_has_leading_zeroes(b, count)
+def solve_challenge(chal):
+    [diff, x] = decode_challenge(chal)
+    y = sloth_root(x, diff, MODULUS)
+    return encode_challenge([y])
 
-    if count == 0:
-        return True
-
-    raise Exception('input too short for requested leading zeroes')
-
-def check_pow(seed, solution, difficulty):
-    digest = hmac.digest(seed, solution, 'sha256')
-    return has_leading_zeroes(digest, difficulty)
+def verify_challenge(chal, sol):
+    [diff, x] = decode_challenge(chal)
+    [y] = decode_challenge(sol)
+    res = sloth_square(y, diff, MODULUS)
+    return (x == res) or (MODULUS - x == res)
 
 def usage():
     sys.stdout.write('Usage:\n')
-    sys.stdout.write('Solve pow: {} solve seed difficulty\n')
-    sys.stdout.write('Check pow: {} ask difficulty\n')
+    sys.stdout.write('Solve pow: {} solve $challenge\n')
+    sys.stdout.write('Check pow: {} ask $difficulty\n')
     sys.stdout.flush()
     sys.exit(1)
 
-def find_pow_solution(seed, difficulty):
-    for i in range((1<<64)-1):
-        solution = i.to_bytes(8, 'little')
-        if check_pow(seed, solution, difficulty):
-            sys.stdout.write("Solution: {}\n".format(solution.hex()))
-            sys.stdout.flush()
-            return True
-
-    return False
-
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 3:
         usage()
+        sys.exit(1)
 
     cmd = sys.argv[1]
 
     if cmd == 'ask':
-        if len(sys.argv) != 3:
-            usage()
-        seed_hex = gen_seed()
         difficulty = int(sys.argv[2])
+
         if difficulty == 0:
             sys.exit(0)
 
         if stdin_is_localhost_socket():
             sys.exit(0)
 
+        challenge = get_challenge(difficulty)
+
         sys.stdout.write("== proof-of-work ==\n")
         sys.stdout.write("please solve a pow first\n")
         sys.stdout.write("You can find the solver at:\n")
         sys.stdout.write("  {}\n".format(SOLVER_URL))
-        sys.stdout.write("./pow.py solve {} {}\n".format(seed_hex, difficulty))
+        sys.stdout.write("./pow.py solve {}\n".format(challenge))
         sys.stdout.write("===================\n")
         sys.stdout.write("\n")
         sys.stdout.write("Solution? ")
         sys.stdout.flush()
-        solution = bytes.fromhex(sys.stdin.readline())
-        seed = bytes.fromhex(seed_hex)
+        solution = ''
+        while not solution:
+            solution = sys.stdin.readline().strip()
 
-        if check_pow(seed, solution, difficulty):
-            sys.stdout.write("Proof-of-work fail")
+        if verify_challenge(challenge, solution):
+            sys.stdout.write("Correct")
             sys.stdout.flush()
             sys.exit(0)
         else:
-            sys.exit(1)
+            sys.stdout.write("Proof-of-work fail")
+            sys.stdout.flush()
 
     elif cmd == 'solve':
-        if len(sys.argv) != 4:
-            usage()
+        challenge = sys.argv[2]
+        solution = solve_challenge(challenge)
 
-        seed = bytes.fromhex(sys.argv[2])
-        difficulty = int(sys.argv[3])
-
-        if find_pow_solution(seed, difficulty):
+        if verify_challenge(challenge, solution):
+            sys.stdout.write("Solution: \n{}\n".format(solution))
+            sys.stdout.flush()
             sys.exit(0)
-        else:
-            sys.exit(1)
-
     else:
         usage()
+
+    sys.exit(1)
 
 if __name__ == "__main__":
     main()
