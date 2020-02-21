1d228b0352815ab3f36efddef11a2d85bb54b91d
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2020-02-21 16:11:02 +0100

    Add GCSFUSE filesystem (#42)
    * Add GCSFUSE filesystem
    
    * Restart daemonset after completion
    
    * grant admin permission to the service account
    
    * delete temp directory
    
    * Move gcsfuse.sh to start.sh
    
    * delete gcsfuse.sh
    
    * delete gsa when stopping cluster

--- infrastructure/kctf/scripts/cluster/start.sh
+++ infrastructure/kctf/scripts/cluster/start.sh
@@ -18,12 +18,37 @@ gcloud compute routers nats create kctf-${CLUSTER_NAME}-nat-config --router-regi
 
 get_cluster_creds
 
-kubectl create configmap gcsfuse-config --namespace kube-system
-kubectl create secret generic gcsfuse-secrets --namespace kube-system
+# GCSFUSE
+
+SUFFIX=$(echo "${PROJECT}-${CLUSTER_NAME}-${ZONE}" | sha1sum)
+BUCKET_NAME="kctf-gcsfuse-${SUFFIX:0:16}"
+GSA_NAME="${BUCKET_NAME}"
+GSA_EMAIL=$(gcloud iam service-accounts list --filter "name:${GSA_NAME}" --format 'get(email)' || true)
+if [ -z "${GSA_EMAIL}" ]; then
+  gcloud iam service-accounts create "${GSA_NAME}" --description "kCTF GCSFUSE service account ${CLUSTER_NAME} ${ZONE}" --display-name "kCTF GCSFUSE ${CLUSTER_NAME} ${ZONE}"
+  GSA_EMAIL=$(gcloud iam service-accounts list --filter "name:${GSA_NAME}" --format 'get(email)')
+fi
+if ! gsutil du "gs://${BUCKET_NAME}/"; then
+  gsutil mb -l eu "gs://${BUCKET_NAME}/"
+  gsutil uniformbucketlevelaccess set on "gs://${BUCKET_NAME}/"
+fi
+gcloud projects add-iam-policy-binding "${PROJECT}" --member "serviceAccount:${GSA_EMAIL}" --role roles/storage.objectAdmin
+
+KEY_PATH=$(mktemp -d)/key.json
+
+gcloud iam service-accounts keys create "${KEY_PATH}" --iam-account "${GSA_EMAIL}"
+
+kubectl create secret generic gcsfuse-secrets --from-file="${KEY_PATH}" --namespace kube-system
+
+rm -rf $(dirname "${KEY_PATH}")
+
+kubectl create configmap gcsfuse-config --from-literal=gcs_bucket="${BUCKET_NAME}" --namespace kube-system
+
+kubectl create -f "${DIR}/config/daemon-gcsfuse.yaml"
+
 
 kubectl create -f "${DIR}/config/apparmor.yaml"
 kubectl create -f "${DIR}/config/daemon.yaml"
-kubectl create -f "${DIR}/config/daemon-gcsfuse.yaml"
 kubectl create -f "${DIR}/config/network-policy.yaml"
 kubectl create -f "${DIR}/config/allow-dns.yaml"
 kubectl patch ServiceAccount default --patch "automountServiceAccountToken: false"
--- infrastructure/kctf/scripts/cluster/stop.sh
+++ infrastructure/kctf/scripts/cluster/stop.sh
@@ -8,3 +8,10 @@ load_config
 
 gcloud container clusters delete ${CLUSTER_NAME}
 gcloud compute routers delete kctf-${CLUSTER_NAME}-nat-router --region ${ZONE::-2}
+
+SUFFIX=$(echo "${PROJECT}-${CLUSTER_NAME}-${ZONE}" | sha1sum)
+GSA_NAME="kctf-gcsfuse-${SUFFIX:0:16}"
+GSA_EMAIL=$(gcloud iam service-accounts list --filter "name:${GSA_NAME}" --format 'get(email)' || true)
+if [ -z "${GSA_EMAIL}" ]; then
+  gcloud iam service-accounts delete "${GSA_EMAIL}"
+fi
