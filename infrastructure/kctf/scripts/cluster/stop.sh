f368860b03501016be0c59fed9362fafb999009f
Eduardo' Vela" Nava (sirdarckcat) <sirdarckcat@gmail.com>
2020-02-24 19:22:21 +0100

    Update gcsfuse to use scoped down bucket permissions (#46)
    * Update gcsfuse to use scoped down bucket permissions
    
    * s/:/=/

--- infrastructure/kctf/scripts/cluster/stop.sh
+++ infrastructure/kctf/scripts/cluster/stop.sh
@@ -7,11 +7,11 @@ source "${DIR}/scripts/lib/config.sh"
 load_config
 
 gcloud container clusters delete ${CLUSTER_NAME}
-gcloud compute routers delete kctf-${CLUSTER_NAME}-nat-router --region ${ZONE::-2}
+gcloud compute routers delete "kctf-${CLUSTER_NAME}-nat-router" --region "${ZONE::-2}" --quiet
 
 SUFFIX=$(echo "${PROJECT}-${CLUSTER_NAME}-${ZONE}" | sha1sum)
 GSA_NAME="kctf-gcsfuse-${SUFFIX:0:16}"
-GSA_EMAIL=$(gcloud iam service-accounts list --filter "name:${GSA_NAME}" --format 'get(email)' || true)
+GSA_EMAIL=$(gcloud iam service-accounts list --filter "name=${GSA_NAME}" --format 'get(email)' || true)
 if [ -z "${GSA_EMAIL}" ]; then
   gcloud iam service-accounts delete "${GSA_EMAIL}"
 fi
