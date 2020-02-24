260394d6234c1773d9e8c610a599d5d8a778af9d
evn <evn@google.com>
2020-02-24 15:20:50 +0100

    add kctf-kubectl

---
+++ infrastructure/kctf/scripts/setup/kubectl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+DIR="$( cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}")" )" >/dev/null && pwd )/../.."
+source "${DIR}/scripts/lib/config.sh"
+
+load_config
+
+kubectl $@
